9c7122aeb003a261ff7abe49baaddfc9fdc03cb8
David <dkeeney@travelbyroad.net>
2014-03-02 08:59:36 -0700

    moved etch to vendor dir

--- js/views/edit-view.js
+++ js/views/edit-view.js
@@ -3,6 +3,11 @@
 (function ($) {
 	'use strict';
 
+    _.extend(etch.config.buttonClasses, {
+        'default': ['bold', 'italic', 'underline', 'save'],
+        'caption': ['bold', 'italic', 'underline', 'link', 'save']
+    });
+
     app.EditView = Backbone.View.extend({
 
         el: '#postform',
@@ -12,8 +17,11 @@
         // Delegated events for creating new items, and clearing completed ones.
         events: {
             'click #post-message': 'postFunction',
-            'click #post-cancel': 'postCancel'
+            'click #post-cancel': 'postCancel',
+            'mousedown .editable': 'editableClick'
         },
+
+        editableClick: etch.editableInit,
 
         // Re-render the titles of the thread item.
         render: function () {
@@ -28,17 +36,13 @@
 
             // todo - validate stuff
             var msg = this.$('[name="new-message"]').val(),
-                subj = this.$('[name="subject"]').val(),
-                a_id = this.$('[name="author"]').val(),
-                t_id = this.$('[name="thread_id"]').val(),
-                m_id = this.$('[name="message_id"]').val(),
-
+                subj = this.model.attributes.subject,
                 newModel = new app.Message({
                     body: msg,
                     title: subj,
-                    thread_id: t_id,
-                    message_id: m_id,
-                    author: a_id
+                    thread_id: this.model.attributes.thread_id,
+                    message_id: this.model.attributes.message_id,
+                    author: app.userId
                 });
 
             newModel.save();
