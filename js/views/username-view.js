cb702c8a38cc0a27ee57248a40f105d5700e67a7
David <dkeeney@travelbyroad.net>
2014-04-01 17:36:43 -0600

    improve error-handling in login name selection

--- js/views/username-view.js
+++ js/views/username-view.js
@@ -3,7 +3,10 @@
 (function ($) {
 	'use strict';
 
-    var R = window.Rdbhost;
+    var R = window.Rdbhost,
+
+        DUPE_KEY = '23505';
+
 
     app.UsernameView = Backbone.View.extend({
 
@@ -17,39 +20,73 @@
         // Re-render the titles of the thread item.
         render: function () {
 
+            var pre;
             this.$el.show();
+
+            if ( ~app.userId.indexOf('@') ) {
+
+                pre = app.userId.split('@')[0];
+                this.$('[name="username"]').val(pre);
+            }
+            else {
+
+                var parts = app.userId.split('/');
+                pre = parts[parts.length-1];
+                if ( ! pre )
+                    pre = parts[2].split('.')[0];
+                this.$('[name="username"]').val(pre);
+            }
+
+            this.$('#su-error').text('');
+
             return this;
         },
 
         postFunction: function(ev) {
 
             var handle = this.$('[name="username"]').val(),
-                handleForm = this;
+                handleForm = this,
+                errorSpan = this.$('#su-error');
 
-            var p = R.preauthPostData({
-                q:  'INSERT INTO users (idx, email_address, handle) \n' +
-                    'SELECT o.idx, NULL, %s FROM auth.openid_accounts o \n' +
-                    ' WHERE o.identifier = %s AND o.key = %s; ',
-                args: [handle, app.userId, app.userKey]
-            });
+            if ( ! handle.trim().length ) {
 
-            p.then(
-                function(resp) {
+                errorSpan.text('Provide a username, please')
+            }
+            else {
 
-                    app.handle = resp.records.rows[0].handle;
-                    $('a.loginLink').attr('href', '#/logout');
-                    $('a.loginLink').text('logout');
-                    // if results, add to display
-                    $('span.user-id').text(app.handle);
-                    window.console.log('name registered as ' + app.handle);
+                var p = R.preauthPostData({
+                    q:  'INSERT INTO users (idx, email_address, handle) \n' +
+                        'SELECT o.idx, NULL, %s FROM auth.openid_accounts o \n' +
+                        ' WHERE o.identifier = %s AND o.key = %s; ',
+                    args: [handle, app.userId, app.userKey]
+                });
 
-                    handleForm.$el.hide();
-                    return this;
-                },
-                function(err) {
-                    alert(err);
-                }
-            );
+                p.then(
+                    function(resp) {
+
+                        app.handle = handle;
+                        $('a.loginLink').attr('href', '#/logout');
+                        $('a.loginLink').text('logout');
+                        // if results, add to display
+                        $('span.user-id').text(app.handle);
+                        window.console.log('name registered as ' + app.handle);
+
+                        handleForm.$el.hide();
+                        errorSpan.text('');
+                    },
+                    function(err) {
+
+                        if ( err[0] === DUPE_KEY  ) {
+
+                            errorSpan.text('That name is in use.  Please choose another.')
+                        }
+                        else {
+
+                            errorSpan.text('Error: ' + err[1]);
+                        }
+                    }
+                );
+            }
             ev.stopPropagation();
             return false;
         }
