08cb4bd7173917a9ceb176211fedaf45371db203
David <dkeeney@travelbyroad.net>
2014-04-04 17:46:00 -0600

    added force to https

--- js/app.js
+++ js/app.js
@@ -5,9 +5,15 @@ var ENTER_KEY = 13,
     ESC_KEY = 27,
     myKeyName = 'OPENID_KEY';
 
+// dummy a console, for ie
 if ( ! window.console )
     window.console = { 'log': function() {} };
 
+// redirect to https, if not there already
+if ( ! ~window.location.host.indexOf('localhost') && window.location.protocol.substr(0,5) !== 'https' )
+    window.location.protocol = 'https';
+
+// create app object, for use in other modules
 var app = _.extend({ userId: undefined, userKey: undefined }, Backbone.Events);
 
 
